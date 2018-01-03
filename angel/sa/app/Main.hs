3bab046c0e15dd319531b9462c6133730ea67c33
dexterlb <dexterlb@qtrp.org>
2018-01-03 17:39:14 +0200

    fix errors

--- angel/sa/app/Main.hs
+++ angel/sa/app/Main.hs
@@ -8,7 +8,7 @@ import qualified Data.Vector.Unboxed as V
 main :: IO ()
 main = do
     let i = addState $ addState $ emptyInter
-    let j = setTransitionsI i [(0, 1, 1), (0, 2, 1), (1, 0, 0)]
+    let j = setTransitionsI i [(0, [('a', 1), ('b', 1)]), (1, [('c', 0)])]
     let k = setLensI (setSlinksI j [(1, 0)]) [(1, 2), (0, 1)]
 
     dotifyToFile "/tmp/sa.dot" k
--- angel/sa/src/Automaton.hs
+++ angel/sa/src/Automaton.hs
@@ -28,45 +28,48 @@ data Inter = Inter {
 
 data Transitions = Transitions {
     withs           :: VU.Vector Char
-    tos             :: VU.Vector Int
+  , tos             :: VU.Vector Int
 }
 
 emptyInter :: Inter
 emptyInter = Inter {
-        transitions = VB.empty
-      , slinks      = VU.empty
-      , lens        = VU.empty
-      , numStates   = 0
+        transitions     = VB.empty
+      , slinks          = VU.empty
+      , lens            = VU.empty
+      , numStates       = 0
+      , epsilonState    = -1
+      , wordState       = -1
     }
 
 emptyTransitions :: Transitions
 emptyTransitions = Transitions {
-        with = VU.empty
-      , to   = VU.empty
+        withs = VU.empty
+      , tos   = VU.empty
     }
 
 addState :: Inter -> Inter
-addState (Inter { transitions, slinks, lens, numStates})
-    = Inter {
-        transitions = V.snoc transitions emptyTransitions
-      , lens        = V.snoc lens 0
-      , slinks      = V.snoc slinks (-1)
-      , numStates   = numStates + 1
-    }
+addState i =
+    let (Inter { transitions, slinks, lens, numStates}) = i in
+        i {
+            transitions = VB.snoc transitions emptyTransitions
+          , lens        = VU.snoc lens 0
+          , slinks      = VU.snoc slinks (-1)
+          , numStates   = numStates + 1
+        }
 
 setStateTransition :: (Char, Int) -> Transitions -> Transitions
 setStateTransition (with, to) (Transitions {withs, tos})
-    | (Just i) <- index, withs VU.! index == with = Transitions {
+    | (Just i) <- index, withs VU.! i == with = Transitions {
             withs = withs
-            tos   = tos VU.// [(index, to)]
+          , tos   = tos VU.// [(i, to)]
         }
     | (Just i) <- index = Transitions {
-            withs = insertVU index with withs
-            tos   = insertVU index   to   tos
+            withs = insertVU i with withs
+          , tos   = insertVU i   to   tos
         }
     | otherwise = Transitions {
             withs = VU.snoc withs with
-            tos   = VU.snoc   tos   to
+          , tos   = VU.snoc   tos   to
         }
     where
         index = VU.findIndex (>= with) withs
@@ -76,29 +79,29 @@ setStateTransitions :: Transitions -- ^ Transitions
                     -> Transitions
 setStateTransitions = foldr setStateTransition
 
-setTransitions :: Vector Transitions
+setTransitions :: VB.Vector Transitions
                -> [(Int, [(Char, Int)])]
-               -> Vector Transitions
+               -> VB.Vector Transitions
 setTransitions transitions updates = transitions VB.//
-    (map (\(from, inner) -> (from, setStateTransitions (transitions VB.! from) inner))
+    (map (\(from, inner) -> (from, setStateTransitions (transitions VB.! from) inner)) updates)
 
 setTransitionsI :: Inter -> [(Int, [(Char, Int)])] -> Inter
 setTransitionsI i l = i { transitions = setTransitions (transitions i) l }
 
-setSlinks :: Vector Int -> [(Int, Int)] -> Vector Int
+setSlinks :: VU.Vector Int -> [(Int, Int)] -> VU.Vector Int
 setSlinks = (VU.//)
 
 setSlinksI :: Inter -> [(Int, Int)] -> Inter
 setSlinksI i l = i { slinks = setSlinks (slinks i) l }
 
-setLens :: Vector Int -> [(Int, Int)] -> Vector Int
+setLens :: VU.Vector Int -> [(Int, Int)] -> VU.Vector Int
 setLens = (VU.//)
 
 setLensI :: Inter -> [(Int, Int)] -> Inter
 setLensI i l = i { lens = setLens (lens i) l }
 
 
-transition :: Vector Transitions
+transition :: VB.Vector Transitions
            -> Int           -- ^ State index
            -> Char          -- ^ Transition index
            -> Maybe Int     -- ^ State which the transition points to
@@ -106,7 +109,7 @@ transition transitions from with = transitionFrom (transitions VB.! from) with
 
 transitionFrom :: Transitions -> Char -> Maybe Int
 transitionFrom (Transitions { withs, tos }) c
-    = (tos VU.!) <$> (VU.findIndex withs c)
+    = (tos VU.!) <$> (VU.elemIndex c withs)
 
 
 decodeLetter :: Char -> Char
@@ -115,7 +118,7 @@ decodeLetter = id
 encodeLetter :: Char -> Char
 encodeLetter = id
 
-transitionsFrom :: Vector Transitions -> Int -> [(Int, Char, Int)]
+transitionsFrom :: VB.Vector Transitions -> Int -> [(Int, Char, Int)]
 transitionsFrom transitions from =
     map (\(with, to) -> (from, with, to)) (zipTransitions (transitions VB.! from))
 
@@ -133,7 +136,7 @@ dotifyState (Inter {transitions, slinks, lens, numStates}) i
     | i == numStates = []
     | otherwise
         = [
-            (show i) ++ " [label=\"" ++ (show i) ++ " len=" ++ (show (lens V.! i)) ++ "\"];"
+            (show i) ++ " [label=\"" ++ (show i) ++ " len=" ++ (show (lens VU.! i)) ++ "\"];"
         ]
         ++ (
             map
@@ -146,7 +149,7 @@ dotifyState (Inter {transitions, slinks, lens, numStates}) i
                     ++ "];")
             $ filter (\(_, _, to) -> to /= (-1))
                 (
-                    (i, "color=\"#40875b\"", (slinks V.! i))
+                    (i, "color=\"#40875b\"", (slinks VU.! i))
                     : map
                         (\(from, with, to) ->
                             (from, transitionLabel lens (from, with, to), to)
@@ -155,15 +158,15 @@ dotifyState (Inter {transitions, slinks, lens, numStates}) i
                 )
         )
 
-transitionLabel :: Vector Int -> (Int, Int, Int) -> String
+transitionLabel :: VU.Vector Int -> (Int, Char, Int) -> String
 transitionLabel lens (from, with, to)
     | lenDiff <= 1 = label
     | otherwise    = label ++ ",style=dotted"
     where
         label   = "label=\"" ++ [(decodeLetter with)]++ "\""
         lenDiff = abs (len1 - len2)
-        len1    = lens V.! from
-        len2    = lens V.! to
+        len1    = lens VU.! from
+        len2    = lens VU.! to
 
 dotifyToFile :: FilePath -> Inter -> IO ()
 dotifyToFile f i = writeFile f (dotify i)
@@ -171,5 +174,5 @@ dotifyToFile f i = writeFile f (dotify i)
 
 -- utils
 
-insertVU :: Int -> a -> VU.Vector a -> VU.Vector a
+insertVU :: (VU.Unbox a) => Int -> a -> VU.Vector a -> VU.Vector a
 insertVU index elem v = (VU.snoc (VU.take index v) elem) VU.++ (VU.drop index v)
