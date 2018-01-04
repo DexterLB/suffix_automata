3a267b669ab21d15773accc45b812e83481ade8e
bitterfly <dageneva@gmail.com>
2018-01-04 20:56:15 +0200

    Make main read from stdin

--- suffix_automata/cmd/suffix_automata/main.go
+++ suffix_automata/cmd/suffix_automata/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/DexterLB/suffix_automata/suffix_automata"
 )
 
 func main() {
-	word := "abcbc"
-	bytestring := []byte(word)
+	// word := "abcbc"
+	// bytestring := []byte(word)
+
+	// d.ProcessCharacter(byte('b'))
+	// fmt.Printf("%s\n========\n", d)
+	// d.Dotify()
+
+	word, _ := ioutil.ReadAll(os.Stdin)
 
 	d := suffix_automata.NewDawg()
-	for _, b := range bytestring {
+	for _, b := range word {
 		d.ProcessCharacter(b)
 	}
 
-	d.ProcessCharacter(byte('b'))
-	fmt.Printf("%s\n========\n", d)
-	d.Dotify()
-
 	states, transitions, finals := d.Count()
-
 	fmt.Printf("States: %d\nTransitions: %d\nFinal states: %d\n", states, transitions, finals)
-
 }
